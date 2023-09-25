57cd96651168b06740f995e116545261110412ab
roychen <royqianchen@tencent.com>
2023-09-25 20:14:48 +0800

    添加字母选中样式以及修复某些异常场景点击不触发callback问题

--- SectionListContacts.js
+++ SectionListContacts.js
@@ -7,14 +7,10 @@ import {
     StyleSheet,
     View,
     Text,
-    ViewPropTypes,
-    Animated,
-    Dimensions,
-    TouchableWithoutFeedback,
     SectionList,
+    TouchableOpacity
 } from 'react-native';
 
-const {width,height} = Dimensions.get('window');
 import {makePy} from "./getFirstAlphabet";
 var _ = require('lodash');
 
@@ -24,8 +20,8 @@ export default class SectionListModule extends Component {
         sectionListData: PropTypes.array.isRequired,//传入的数据
         sectionHeight: PropTypes.number,//内容的高度
         sectionHeaderHeight: PropTypes.number,//头部索引的高度
-        letterViewStyle: ViewPropTypes.style,//右边字母组件样式
-        sectionItemViewStyle: ViewPropTypes.style,//item组件样式
+        letterViewStyle: PropTypes.object,//右边字母组件样式
+        sectionItemViewStyle: PropTypes.object,//item组件样式
         sectionItemTextStyle: Text.propTypes.style,//item文字样式
         sectionHeaderTextStyle: Text.propTypes.style,//头部文字样式
         scrollAnimation:PropTypes.bool,//是否启动动画
@@ -38,12 +34,16 @@ export default class SectionListModule extends Component {
         sectionHeaderHeight: 25,
         scrollAnimation: false,
         showAlphabet: true,
-        otherAlphabet: '其他'
+        otherAlphabet: '其他',
+        activeOpacity: 0.5,
+        highlightAlphabet: '',
+        highlightAlphabetColor: 'blue',
+        showHighlightAlphabetColor: true
     };
 
     constructor(props) {
         super(props);
-        var data=[
+        var data = [
             {data: [],key: 'A'},
             {data: [],key: 'B'},
             {data: [],key: 'C'},
@@ -79,25 +79,25 @@ export default class SectionListModule extends Component {
 
     filterData(){
         var data = _.cloneDeep(this.state.dataArray);
-        this.props.sectionListData.map((item,index)=>{
-            for (let i=0;i<data.length;i++){
-                if (i==data.length-1){
-                    data[i].data.push(item)
-                    break
-                }else if (data[i].key==makePy(item.name.toUpperCase())){
-                    data[i].data.push(item)
-                    break
+        this.props.sectionListData.map((item,index) => {
+            for (let i = 0;i < data.length;i++){
+                if (i == data.length-1){
+                    data[i].data.push(item);
+                    break;
+                }else if (data[i].key == makePy(item.name.toUpperCase())){
+                    data[i].data.push(item);
+                    break;
                 }else {
-                    continue
+                    continue;
                 }
             }
         })
-        let delData = []
-        let letterData = []
+        let delData = [];
+        let letterData = [];
         for (var i in data){
-            if (data[i].data.length!=0){
-                delData.push(data[i])
-                letterData.push(data[i].key)
+            if (data[i].data.length != 0){
+                delData.push(data[i]);
+                letterData.push(data[i].key);
             }
         }
         return{
@@ -107,70 +107,99 @@ export default class SectionListModule extends Component {
     }
 
     render() {
-        let filterData=this.filterData()
-        let delData = filterData.delData
-        let letterData = filterData.letterData
-
+        let filterData = this.filterData();
+        let delData = filterData.delData;
+        let letterData = filterData.letterData;
+        if (!this.state.highlightAlphabet && letterData.length > 0) {
+            this.state.highlightAlphabet = letterData[0];
+        }
         return(
             <View style={styles.container}>
                 <SectionList
                     {...this.props}
-                    style={this.props.SectionListStyle}
-                    ref={s=>this.sectionList=s}
-                    keyExtractor={this._keyExtractor}
-                    sections={delData}
-                    renderSectionHeader={this._renderSectionHeader}
-                    renderItem={this._renderItem}
-                    getItemLayout={(data, index) => ( {length: this.props.sectionHeight, offset: this.props.sectionHeight * index, index} )}
+                    style = {this.props.SectionListStyle}
+                    ref = {s => this.sectionList = s}
+                    keyExtractor = {this._keyExtractor}
+                    sections = {delData}
+                    renderSectionHeader = {this._renderSectionHeader}
+                    renderItem = {this._renderItem}
+                    onViewableItemsChanged = {this._onViewableItemsChanged}
+                    getItemLayout = {(data, index) => ( {length: this.props.sectionHeight, offset: this.props.sectionHeight * index, index} )}
+                    onMomentumScrollEnd={()=>{
+                        this.setState({highlightAlphabet:this.state.highlightAlphabet});
+                    }}
                 />
                 {
                     this.props.showAlphabet?(
-                            <View style={[styles.letterView,this.props.letterViewStyle]}>
-                                {
-                                    letterData.map((item,index)=>{
-                                        let otherStyle=[]
-                                        if (index==letterData.length-1){
-                                            if (item==this.props.otherAlphabet){
-                                                otherStyle.push({width: 20})
-                                            }
+                        <View style={[styles.letterView,this.props.letterViewStyle]}>
+                            {
+                                letterData.map((item,index) => {
+                                    let otherStyle = [];
+                                    if (index == letterData.length-1){
+                                        if (item == this.props.otherAlphabet){
+                                            otherStyle.push({width: 20});
                                         }
-                                        return(
-                                            <TouchableWithoutFeedback key={'letter_'+index} onPress={()=>{
-                                                this.sectionList.scrollToLocation({animated: this.props.scrollAnimation, itemIndex: 0,sectionIndex: index,viewOffset: (this.props.sectionHeight * (index + 1)) + (this.props.sectionHeaderHeight * index)})
-                                            }}>
-                                                <View style={[styles.letterItemView,otherStyle]}>
-                                                    <Text numberOfLines={0} style={[styles.letterText,this.props.letterTextStyle]}>{item}</Text>
-                                                </View>
-                                            </TouchableWithoutFeedback>
-                                        )
-                                    })
-                                }
-                            </View>
-                        ):(
-                            <View></View>
-                        )
+                                    }
+                                    let alphabetColor = [];
+                                    if (this.state.highlightAlphabet === item && this.props.showHighlightAlphabetColor) {
+                                        otherStyle.push({color: this.props.highlightAlphabetColor || 'blue'});
+                                    }
+
+                                    return(
+                                        <TouchableOpacity activeOpacity = {this.props.activeOpacity} key = {'letter_'+index} onPress = {() => {
+                                            this.sectionList.scrollToLocation({animated: this.props.scrollAnimation, itemIndex: 0,sectionIndex: index,viewOffset: (this.props.sectionHeight * (index + 1)) + (this.props.sectionHeaderHeight * index)});
+                                            this.setState({
+                                                highlightAlphabet: item
+                                            })
+                                        }}>
+                                            <View style = {[styles.letterItemView,otherStyle]}>
+                                                <Text numberOfLines = {0} style = {[styles.letterText,this.props.letterTextStyle,otherStyle]}>{item}</Text>
+                                            </View>
+                                        </TouchableOpacity>
+                                    )
+                                })
+                            }
+                        </View>
+                    ):(
+                        <View></View>
+                    )
                 }
             </View>
         )
     }
 
-    _renderSectionHeader=({section})=>{
+    _onViewableItemsChanged = ({ viewableItems, changed }) => {
+        let keyStr = '';
+        const visibleSections = viewableItems.filter(item => item.section).map(item => item.section);
+        for (let i = 0;i < visibleSections.length;i++) {
+            if (keyStr){
+                break;
+            } else {
+                if (visibleSections[i].key) {
+                    keyStr = visibleSections[i].key;
+                }
+            }
+        }
+        this.state.highlightAlphabet = keyStr;
+    }
+
+    _renderSectionHeader = ({section}) => {
         if (this.props.renderHeader){
             return(
                 this.props.renderHeader(section)
             )
         }
         return(
-            <View style={[styles.sectionHeaderView, {height: this.props.sectionHeaderHeight}]}>
-                <Text style={[styles.sectionHeaderText,this.props.sectionHeaderTextStyle]}>{section.key}</Text>
-                <View style={styles.lineView}></View>
+            <View style = {[styles.sectionHeaderView, {height: this.props.sectionHeaderHeight}]}>
+                <Text style = {[styles.sectionHeaderText,this.props.sectionHeaderTextStyle]}>{section.key}</Text>
+                <View style = {styles.lineView}></View>
             </View>
         )
     }
 
     _keyExtractor = (item, index) => index;
 
-    _renderItem=({item,index,section})=>{
+    _renderItem = ({item,index,section}) => {
 
         if (this.props.renderItem){
             return(
@@ -180,10 +209,8 @@ export default class SectionListModule extends Component {
         return(
             <SectionItem
                 {...this.props}
-                callback={()=>{
-                    this.props.SectionListClickCallback(item,index,section)
-                }}
-                item={item} ></SectionItem>
+                callback = {() => {this.props.SectionListClickCallback(item,index,section)}}
+                item = {item} />
         )
     }
 
@@ -192,13 +219,13 @@ export default class SectionListModule extends Component {
 class SectionItem extends PureComponent {
     render() {
         return(
-            <TouchableWithoutFeedback onPress={()=>{
-                this.props.callback()
+            <TouchableOpacity activeOpacity = {this.props.activeOpacity} onPress={() => {
+                this.props.callback();
             }}>
-                <View style={[styles.itemStyle,this.props.sectionItemViewStyle]}>
-                    <Text style={[styles.artistText,this.props.sectionItemTextStyle]}>{this.props.item.name}</Text>
+                <View style = {[styles.itemStyle,this.props.sectionItemViewStyle]}>
+                    <Text style = {[styles.artistText,this.props.sectionItemTextStyle]}>{this.props.item.name}</Text>
                 </View>
-            </TouchableWithoutFeedback>
+            </TouchableOpacity>
 
         )
     }
@@ -251,7 +278,9 @@ const styles = StyleSheet.create({
         color: '#333333'
     },
     letterText: {
+        minWidth: 20,
         fontSize: 15,
-        color: '#333333'
+        color: '#333333',
+        textAlign: 'center',
     }
 });
